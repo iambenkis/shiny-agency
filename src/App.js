3dbb44dcc3d7f63f65053a73075cc35f2590258e
benkis <benkisenge03@gmail.com>
2023-07-26 18:20:07 +0200

    Update App component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
 import logo from './logo.svg'
 import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Survey from './pages/Survey'
+import Header from './components/Header'
+import Error from './components/Error'
+import Results from './pages/Results'
+import Freelances from './pages/Freelances'
+import colors from './colors'
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+  div {
+    font-family: 'Trebuchet MS', Helvetica, sans-serif;
+  }
+`
 
 function App() {
-    let msg = 11
-    console.log(msg)
-    return <div className="App"></div>
+  return (
+    <div className="App">
+      <Router>
+        <GlobalStyle />
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/survey/:questionNumber" element={<Survey />} />
+          <Route path="/results/" element={<Results />} />
+          <Route path="/freelances/" element={<Freelances />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </Router>
+    </div>
+  )
 }
 
 export default App
