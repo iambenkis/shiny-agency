d424b1f956c8346b5dc6c584f9e1506bfc143f08
benkis <benkisenge03@gmail.com>
2023-07-26 18:23:20 +0200

    Add css in js to Home component

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,59 @@
+import { useState } from 'react'
+import styled from 'styled-components'
+import { StyledLink } from '../../components/Header'
+import homeImg from '../../assets/undraw_Resume_re_hkth 1.png'
+
+const HomeContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  background-color: #f9f9fc;
+  margin: 0 6rem;
+  height: 86.5vh;
+`
+
+const HomeWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 0 3rem;
+  aign-items: center;
+`
+
+const HomeText = styled.div`
+  width: 33%;
+  display: flex;
+  flex-direction: column;
+  align-items: start;
+  text-align: start;
+  line-height: 1.5;
+  marginleft: 5rem;
+`
+
+const StyleHomeImg = styled.img`
+  width: 30%;
+`
+
+const Home = () => {
+  const [size, setSize] = useState(1)
+
+  return (
+    <HomeContainer>
+      <HomeWrapper>
+        <HomeText>
+          <h1>
+            Reperez vos besoins, on s'occupe du reste, avec nos meilleurs
+            talents
+          </h1>
+          <StyledLink to="/survey/1" $isRounded>
+            Faire le text
+          </StyledLink>
+        </HomeText>
+        <StyleHomeImg src={homeImg} alt="home-img"></StyleHomeImg>
+      </HomeWrapper>
+    </HomeContainer>
+  )
+}
+
+export default Home
